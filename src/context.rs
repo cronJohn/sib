389d49a8835c33e47be9ebedaa71fbd5c3bc2d79
cronJohn <51012201+cronJohn@users.noreply.github.com>
2026-04-14 22:39:39 -0500

    Remove legacy NoteService

--- src/context.rs
+++ src/context.rs
@@ -1,12 +1,9 @@
 use crate::{
     config::Config,
-    services::{
-        editor::EditorService, note::NoteService, parse::ParseService, ranker::RankerService,
-    },
+    services::{editor::EditorService, parse::ParseService, ranker::RankerService},
 };
 
 pub struct Context {
-    pub notes: NoteService,
     pub editor: EditorService,
     pub parser: ParseService,
     pub ranker: RankerService,
@@ -15,7 +12,6 @@ pub struct Context {
 impl Context {
     pub fn new(cfg: &Config) -> Self {
         Self {
-            notes: NoteService::new(cfg.base_notes_dir.clone()),
             editor: EditorService::new(cfg.editor.clone()),
             parser: ParseService::new(cfg.base_notes_dir.clone()),
             ranker: RankerService::new(cfg.usage_file.clone()),
--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,4 +1,3 @@
 pub mod editor;
-pub mod note;
 pub mod parse;
 pub mod ranker;
